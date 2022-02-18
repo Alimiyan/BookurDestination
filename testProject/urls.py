b20b1266c3aadc2fd8d948fd53fed01dbc1f3fe9
alimiyan <alimiyan123@gmail.com>
2022-02-18 23:01:18 +0530

    connected to db

--- testProject/urls.py
+++ testProject/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('',include('calc.urls')),
+    path('',include('destination.urls')),
     path('admin/', admin.site.urls),
 ]
